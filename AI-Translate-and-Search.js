17b024d0df3d1df056d00eb376a85c1da3fa412f
butaixianran <butaixianran@qq.com>
2021-12-08 10:51:23 +0800

    脚本改名，改为中文名

--- AI-Translate-and-Search.js
+++ AI-Translate-and-Search.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         AI Translate and Search
+// @name         AI翻译和搜索
 // @namespace    butaixianran
-// @version      0.6
+// @version      0.7
 // @description  划词AI翻译+搜索。AI翻译按质量排序。调用：金山词霸，阿里翻译，百度翻译，搜狗翻译，腾讯翻译，彩云小译，DeepL，沪江日语词典等。搜索包含：知乎，b站，百科，youtube，twitter等。去掉了原作者提供的大量不常用词典。
 // @author       barrer, modified by butaixianran
 // @homepage     https://github.com/butaixianran/AI-Translate-and-Search
